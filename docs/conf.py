3757aa6898fd59540d9a3aec79d52ece85047282
dabadav <dante.avino@alum.esci.upf.edu>
2023-06-13 18:17:54 +0200

    Publish Docs

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@ sys.path.insert(0, os.path.abspath("../pipeline_vsdi"))
 project = 'VSDI Pipeline'
 copyright = '2023, NeuroNetMem'
 author = 'NeuroNetMem'
-release = '1.0.0'
+release = '0.1.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
